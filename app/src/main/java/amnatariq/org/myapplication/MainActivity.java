1616e7c95cf52785ea9551cfd239036f54625297
Amna <aamnaht4@gmail.com>
2022-11-10 21:44:01 +0500

    buttons and text views added in mainactivity.java

---
+++ app/src/main/java/amnatariq/org/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package amnatariq.org.myapplication;
+
+import static java.util.Collections.shuffle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
